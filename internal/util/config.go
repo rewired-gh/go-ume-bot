2c64c16672dce6c8f7d99ce7beb7f2ee963bf40d
Takehana <cowpowermax@pm.me>
2023-10-23 21:28:00 +0800

    fix: config strategy

--- internal/util/config.go
+++ internal/util/config.go
@@ -10,15 +10,17 @@ type Config struct {
 
 // Reads configuration from environment file then environment variables
 func LoadConfig(path string) (config Config, err error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
-	viper.SetConfigType("env")
-
 	viper.AutomaticEnv()
 
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
+	if err = viper.Unmarshal(&config); err != nil {
+		viper.SetConfigName("app")
+		viper.SetConfigType("env")
+		viper.AddConfigPath(path)
+
+		err = viper.ReadInConfig()
+		if err != nil {
+			return
+		}
 	}
 
 	err = viper.Unmarshal(&config)
